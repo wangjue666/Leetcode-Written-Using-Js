560f8b1fc2c4c3a725235a78bdd2f6e09026643c
wangjue666 <3262629206@qq.com>
2019-01-29 10:55:56 +0800

    完成2的幂算法

--- 2的幂.js
+++ 2的幂.js
@@ -1,3 +1,14 @@
 var isPowerOfTwo = function(n) {
-    
+    if(n==1) {
+        return true
+    }
+    while(n>1) {
+        n = n/2
+        if(n%1>0) {
+            return false
+        }
+    }
+    return n==1
 };
+
+console.log(isPowerOfTwo(6))
