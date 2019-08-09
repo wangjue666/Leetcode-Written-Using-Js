19cda369bd4610ec54c2186497d0d0787e185e96
wangjue666 <3262629206@qq.com>
2019-08-09 09:11:50 +0800

    查询后的偶数和

--- 查询后的偶数和.js
+++ 查询后的偶数和.js
@@ -1,11 +1,22 @@
-给出一个整数数组 A 和一个查询数组 queries。
-
-对于第 i 次查询，有 val = queries[i][0], 
-
-index = queries[i][1]，我们会把 val 加到 A[index] 上。
-
-然后，第 i 次查询的答案是 A 中偶数值的和。
-
-（此处给定的 index = queries[i][1] 是从 0 开始的索引，每次查询都会永久修改数组 A。）
-
-返回所有查询的答案。你的答案应当以数组 answer 给出，answer[i] 为第 i 次查询的答案。
+var sumEvenAfterQueries = function(A, queries) {
+    let num = 0;
+    let target =[]
+    A.forEach(it=>{
+        if(it%2 == 0){
+            num +=it;
+        }
+    })
+    A.forEach((it,k)=>{
+        let x =A[queries[k][1]];
+        let y = x + queries[k][0];
+            A[queries[k][1]] =y;
+        if(x%2 == 0){
+            num -=x;
+        }
+        if(y%2 == 0){
+            num +=y
+        }
+        target.push(num)
+    })
+    return target;
+};
