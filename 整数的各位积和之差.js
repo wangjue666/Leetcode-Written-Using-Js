80cb9079e4feb07ccaa9361ff7341ac12a5d8983
王珏 <wangjuesix@gmail.com>
2020-11-05 16:42:53 +0800

    Update 整数的各位积和之差.js

--- 整数的各位积和之差.js
+++ 整数的各位积和之差.js
@@ -1,7 +1,6 @@
 var subtractProductAndSum = function(n) {
     let arr = n.toString().split('')
     let product = arr.reduce((pre, cur) => pre * cur, 1);
-    console.log(product)
     let sum = arr.reduce((pre,cur)=>Number(pre) + Number(cur), 0 );
     return product - sum
 };
